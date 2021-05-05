d981684579f4582651ac93e6cab17f251bd19ea9
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-05 10:24:02 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 03 - DATETIME/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace EXERCISE_03___DATETIME {
+    /// -------> START / MAIN 
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            DateTime x = ...
+            DateTime y = x.Add(timeSpan);
+            DateTime y = x.AddDays(double);
+            DateTime y = x.AddHours(double);
+            DateTime y = x.AddMilliseconds(double);
+            DateTime y = x.AddMinutes(double);
+            DateTime y = x.AddMonths(int);
+            DateTime y = x.AddSeconds(double);
+            DateTime y = x.AddTicks(long);
+            DateTime y = x.AddYears(int);
+            DateTime y = x.Subtract(timeSpan);
+            TimeSpan t = x.Subtract(dateTime)
+        }
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    /// -------> END
+}
